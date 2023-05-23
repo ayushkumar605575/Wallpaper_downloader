a537d9ace7f6e2eda44f7d2e6b4d4e223db4a283
Ayush Kumar <112181652+ayushkumar605575@users.noreply.github.com>
2023-05-23 18:32:43 +0530

    Add files via upload

--- Wallpaper Changer.py
+++ Wallpaper Changer.py
@@ -1,36 +1,35 @@
-from os import listdir, remove, mkdir
-from random import randint, choices, shuffle, choice
+import contextlib
+from os import listdir, mkdir, remove
+from random import choice, choices, randint, shuffle
+from threading import Thread
+from time import time
+
+from bs4 import BeautifulSoup as bs
 from colorama import Fore, init
 from requests import get as G
-from threading import Thread
-from bs4 import BeautifulSoup as bs
-from time import time, sleep
+
 tt = time()
 init(True)
 def Banner():
     banner = '''
                 _______    _          _          _______    _______    _______    _______    _______   
-    |\     /|  (  ___  )  ( \        ( \        (  ____ )  (  ___  )  (  ____ )  (  ____ \  (  ____ )  
-    | )   ( |  | (   ) |  | (        | (        | (    )|  | (   ) |  | (    )|  | (    \/  | (    )|  
+    |\\     /|  (  ___  )  ( \\        ( \\        (  ____ )  (  ___  )  (  ____ )  (  ____ \\  (  ____ )  
+    | )   ( |  | (   ) |  | (        | (        | (    )|  | (   ) |  | (    )|  | (    \\/  | (    )|  
     | | _ | |  | (___) |  | |        | |        | (____)|  | (___) |  | (____)|  | (__      | (____)|  
     | |( )| |  |  ___  |  | |        | |        |  _____)  |  ___  |  |  _____)  |  __)     |     __)  
-    | || || |  | (   ) |  | |        | |        | (        | (   ) |  | (        | (        | (\ (     
-    | () () |  | )   ( |  | (____/\  | (____/\  | )        | )   ( |  | )        | (____/\  | ) \ \__  
-    (_______)  |/     \|  (_______/  (_______/  |/         |/     \|  |/         (_______/  |/   \__/                                                                                                         
+    | || || |  | (   ) |  | |        | |        | (        | (   ) |  | (        | (        | (\\ (     
+    | () () |  | )   ( |  | (____/\\  | (____/\\  | )        | )   ( |  | )        | (____/\\  | ) \\ \\__  
+    (_______)  |/     \\|  (_______/  (_______/  |/         |/     \\|  |/         (_______/  |/   \\__/                                                                                                         
     '''
-    ver = "        Version : 3.4.6"
-    cre = "        Developer: Ayush Kumar"
     print(Fore.LIGHTWHITE_EX+banner)
-    print(Fore.LIGHTWHITE_EX+'      <<<<<<<<<<<<<>>>>>>>>>>>>>')                   
-    print(Fore.LIGHTBLUE_EX+ ver)
-    print(Fore.LIGHTRED_EX+ cre)
-    print(Fore.LIGHTWHITE_EX+'      <<<<<<<<<<<<<>>>>>>>>>>>>>')
+    print(f'{Fore.LIGHTWHITE_EX}      <<<<<<<<<<<<<>>>>>>>>>>>>>')
+    print(f"{Fore.LIGHTBLUE_EX}        Version : 3.5.6")
+    print(f"{Fore.LIGHTRED_EX}        Developer: Ayush Kumar")
+    print(f'{Fore.LIGHTWHITE_EX}      <<<<<<<<<<<<<>>>>>>>>>>>>>')
 
-try:
+with contextlib.suppress(Exception):
     mkdir('./Py_wallpaper')
     print('Setting up things for the first time!!!')
-except:
-    pass
 tmp_link, final_links, threads, old_wp, category = [], [], [], listdir('./Py_wallpaper'), ['abstract','technology','minimalism','futuristic','digital','art','pattern','illustration','creativity','illuminated','artwork','space','sky','architecture']
 def wp_flare():
     cho_ice = str(choice(category))
@@ -38,18 +37,15 @@ def wp_flare():
     wp = G(''.join(['https://www.wallpaperflare.com/search?wallpaper=',cho_ice,'&page=',str(randint(1,80))]),timeout=5).content
     soup = bs(wp,'lxml')
     lnk = soup.find_all('a',attrs={'itemprop':'url'})
-    for l in lnk:
-        # if "anime" not in l['href']:
-            tmp_link.append(''.join([l['href'],'/download']))
+    for i in lnk:
+        tmp_link.append(''.join([i['href'],'/download']))
 
 def wp_flare_download(dl_links):
-    fetch = G(dl_links).content
+    fetch = G(dl_links,timeout=10).content
     soup = bs(fetch,'lxml')
-    try:
-        dl_lnk = soup.find('img',attrs={'itemprop':"contentUrl"})['src']
+    with contextlib.suppress(Exception):
+        dl_lnk = soup.find('img',attrs={'itemprop':"contentUrl"})['src'] # type: ignore
         final_links.append(dl_lnk)
-    except:
-        pass
 Banner()
 wp_flare()
 # wp_flare()
@@ -63,19 +59,20 @@ for l in tmp_link:
 for thread in threads:
     thread.join()
 
-def thrd(link):
-    r = G(link, stream=True).content
-    with open(''.join(['./Py_wallpaper/',link.split("/")[-1]]), 'wb') as f:
+def thrd(lnk):
+    r = G(lnk, stream=True,timeout=10).content
+    with open(''.join(['./Py_wallpaper/',lnk.split("/")[-1]]), 'wb') as f:
         f.write(r)
 
 for link in final_links:
     t = Thread(target=thrd, args=(link,))
     t.start()
     threads.append(t)
+
+for old_wlp in old_wp:
+    remove(f'./Py_wallpaper/{str(old_wlp)}')
+
 for thread in threads:
     thread.join()
 
-for old_wlp in old_wp:
-   remove('./Py_wallpaper/'+str(old_wlp))
 print(time()-tt)
-sleep(2)
